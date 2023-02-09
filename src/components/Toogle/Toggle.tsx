31dde68d4f702c1971db2d8ea7d1e44b3dca741d
MohammedJR <mohammed.j@hotelistan.com>
2023-02-09 10:11:15 +0300

    feat: the questions rows

--- src/components/Toogle/Toggle.tsx
+++ src/components/Toogle/Toggle.tsx
@@ -19,10 +19,13 @@ const sampleOptions: ToggleOptionType[] = [
 interface TooglePropsType {
     options?: ToggleOptionType[],
     onChange?: (option: ToggleOptionType) => void 
+    outerWrapperClassNames?: string,
+    labelClassNames?: string,
+    optionWrapperClassNames?: string;
 }
 
 const Toggle: FC<TooglePropsType> = (props) => {
-    const { options: optionsFromProps, onChange } = props
+    const { options: optionsFromProps, onChange, outerWrapperClassNames, labelClassNames, optionWrapperClassNames } = props
     const [options, setOptions] = useState(optionsFromProps ?? sampleOptions)
     const [activeOption, setActiveOption] = useState(options[0])
     const handleChange = (option: ToggleOptionType) => {
@@ -32,15 +35,17 @@ const Toggle: FC<TooglePropsType> = (props) => {
 
     
   return (
-    <div className='bg-white rounded-[10px] p-[10px] relative '>
+    <div className={`${outerWrapperClassNames} bg-white rounded-[10px] p-[10px] relative `}>
         <div className='grid grid-cols-2 relative'>
                 <div className={`absolute w-1/2 h-full z-0 bg-[#800000] border border-orange-400 transition-all rounded-[5px]  ${activeOption.value === options[1].value ? 'translate-x-full' : ''}`}></div>
             {
             options.slice(0,2).map((option, index) => {
                 const {label, value, id} = option
                 return (
-                    <div className={`h-full bg-transparent flex items-center justify-center relative z-10 transition-all py-3 px-11 ${activeOption.value === options[index].value  ? 'text-white' : '' }`} onClick={() => {handleChange(option)}}>
+                    <div className={`${optionWrapperClassNames} h-full bg-transparent flex items-center justify-center relative z-10 transition-all py-3 px-11 ${activeOption.value === options[index].value  ? 'text-white' : '' }`} onClick={() => {handleChange(option)}} key={id}>
+                        <span className={`${labelClassNames}`}>
                             {label}
+                        </span> 
                     </div>
 
                 )
--- src/components/UserHealthInformationRegistry/UserHealthInformationRegistry.tsx
+++ src/components/UserHealthInformationRegistry/UserHealthInformationRegistry.tsx
@@ -1,8 +1,168 @@
 import React from 'react';
+import { ToggleOptionType } from '../../types';
+import Toggle from '../Toogle/Toggle';
+
+interface SampleQuestion {
+  title: string,
+  subtitle: string,
+  options: ToggleOptionType[],
+  id: string
+}
+
+
+const sampleQuestions: SampleQuestion[] = [
+  {
+    title: 'Heart Proplems',
+    subtitle: 'Kalp Problemleri',
+    options: [
+      {
+        label: 'Yes / Evet',
+        value: 'yes',
+        id: '1'
+      },
+      {
+        label: 'No / Hayır',
+        value: 'no',
+        id: '2'
+
+      }
+    ],
+    id: '1'
+  },
+  {
+    title: 'Heart Proplems',
+    subtitle: 'Kalp Problemleri',
+    options: [
+      {
+        label: 'Yes / Evet',
+        value: 'yes',
+        id: '1'
+      },
+      {
+        label: 'No / Hayır',
+        value: 'no',
+        id: '2'
+
+      }
+    ],
+    id: '2'
+  },
+  {
+    title: 'Heart Proplems',
+    subtitle: 'Kalp Problemleri',
+    options: [
+      {
+        label: 'Yes / Evet',
+        value: 'yes',
+        id: '1'
+      },
+      {
+        label: 'No / Hayır',
+        value: 'no',
+        id: '2'
+
+      }
+    ],
+    id: '3'
+  },
+  {
+    title: 'Heart Proplems',
+    subtitle: 'Kalp Problemleri',
+    options: [
+      {
+        label: 'Yes / Evet',
+        value: 'yes',
+        id: '1'
+      },
+      {
+        label: 'No / Hayır',
+        value: 'no',
+        id: '2'
+
+      }
+    ],
+    id: '4'
+  },
+  {
+    title: 'Heart Proplems',
+    subtitle: 'Kalp Problemleri',
+    options: [
+      {
+        label: 'Yes / Evet',
+        value: 'yes',
+        id: '1'
+      },
+      {
+        label: 'No / Hayır',
+        value: 'no',
+        id: '2'
+
+      }
+    ],
+    id: '5'
+  },
+  {
+    title: 'Heart Proplems',
+    subtitle: 'Kalp Problemleri',
+    options: [
+      {
+        label: 'Yes / Evet',
+        value: 'yes',
+        id: '1'
+      },
+      {
+        label: 'No / Hayır',
+        value: 'no',
+        id: '2'
+
+      }
+    ],
+    id: '6'
+  },
+  {
+    title: 'Heart Proplems',
+    subtitle: 'Kalp Problemleri',
+    options: [
+      {
+        label: 'Yes / Evet',
+        value: 'yes',
+        id: '1'
+      },
+      {
+        label: 'No / Hayır',
+        value: 'no',
+        id: '2'
+
+      }
+    ],
+    id: '7'
+  },
+]
+
 
 const UserHealthInformation = () => {
+  
   return (
-    <div>UserHealthInformation</div>
+    <div className='w-full flex flex-col px-9 border-2 border-orange-400 pt-5'>
+        {
+          sampleQuestions.map(question => {
+            const {title, subtitle, options,id} = question
+            return(
+              <div className='flex items-center justify-between mb-5 border-2 border-pink-500 ' key={id}>
+                <div className='flex flex-col'>
+                  <span className='text-base font-semibold'>
+                    {title}
+                  </span>
+                  <span className='text-xs text-normal'>
+                    {subtitle}
+                  </span>
+                </div>
+                <Toggle options={options} outerWrapperClassNames='max-w-[240px]' labelClassNames='text-xs font-semibold' optionWrapperClassNames='px-5'/>
+              </div>
+            )
+          })
+        }
+    </div>
   )
 };
 
