d415911d81e03bea29e6feca317c2efaeed39746
MohammedJR <mohammed.j@hotelistan.com>
2023-02-09 12:17:41 +0300

    first step default layout

--- src/components/UserPersonalDataRegistry/UserPersonalDataRegistry.tsx
+++ src/components/UserPersonalDataRegistry/UserPersonalDataRegistry.tsx
@@ -1,41 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { ToggleOptionType } from '../../types';
-import Toggle from '../Toogle/Toggle';
-
-
-const sampleOptions: ToggleOptionType[] = [
-  {
-      label: 'Male / Erkek',
-      value: 'male',
-      id: '0'
-  },
-  {
-      label: 'Female / kadın',
-      value: 'female',
-      id: '1'
-  },
-
-]
-
-
+import React from 'react';
 
 const UserPersonalDataRegistry = () => {
-  const [ selectedGender, setSelectedGender ] = useState<ToggleOptionType>()
-
-  const handleToggle = (gender?: ToggleOptionType) => {
-    setSelectedGender(gender)
-  }
-
-  useEffect(() => {
-    console.log('this is the selected gender: ', selectedGender)
-  }, [selectedGender])
-  
-
   return (
-    <div>
-      UserPersonalDataRegistry
-      <Toggle options={sampleOptions} onChange={(e) => handleToggle(e)} />  
-    </div>
+    <div>UserPersonalDataRegistry</div>
   )
 };
 
