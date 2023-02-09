ebdc94effef64caab55f50588e3caa52c31b5973
Mohammad Al Saleh <mo.alsaleh95@gmail.com>
2023-02-09 15:08:47 +0300

    progress CMH-11

--- src/components/UserPersonalDataRegistry/UserPersonalDataRegistry.tsx
+++ src/components/UserPersonalDataRegistry/UserPersonalDataRegistry.tsx
@@ -1,38 +1,24 @@
 import React from 'react';
-import logo_1 from "../../assets/logo/logo_1.svg";
-import logo_2 from "../../assets/logo/logo_2.svg";
-import logo_3 from "../../assets/logo/logo_3.svg";
-import logo_4 from "../../assets/logo/logo_4.svg";
+import icon_person from "../../assets/icons/icon_1.svg";
+
+import icon_email from "../../assets/icons/icon_3.svg";
+import icon_date from "../../assets/icons/icon_4.svg";
 import AnimatedInput from './AnimatedInput';
-
-import { useState } from 'react';
-import { PhoneInput } from 'react-international-phone';
-import 'react-international-phone/style.css';
-
+import TelInput from './TelInput';
 const UserPersonalDataRegistry = () => {
 
-  const [phone, setPhone] = useState('');
-  
   return (
     <div className='container '>
       <form className='grid grid-cols-2 gap-10 mt-8'>
 
-        <AnimatedInput label={'Name Surname / Adınız Soyadınız'} inputType={'text'} logo={logo_1} />
+        <AnimatedInput label={'Name Surname / Adınız Soyadınız'} inputType={'text'} logo={icon_person} />
 
-        <div className='bg-white py-3 px-8 rounded-2xl flex justify-start items-center'>
-          <img className='mr-6 w-5' src={logo_2} alt="logo_2" />
-          <label className='text-xs font-semibold'>
-            Phone Number / Telefon Numaranız 
-            <PhoneInput
-              initialCountry="tr"
-              value={phone}
-              onChange={(phone) => setPhone(phone)}
-            />
-          </label>
-        </div> 
+        
+            <TelInput />
+          
 
-        <AnimatedInput label={'E-mail Address / E-posta Adresiniz'} inputType={'email'} logo={logo_3} />
-        <AnimatedInput label={'Birthday / Doğum Tarihiniz'} inputType={'date'} logo={logo_4} />
+        <AnimatedInput label={'E-mail Address / E-posta Adresiniz'} inputType={'email'} logo={icon_email} />
+        <AnimatedInput label={'Birthday / Doğum Tarihiniz'} inputType={'date'} logo={icon_date} />
         
       </form>
     </div>
