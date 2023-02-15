b28cdcbc4ccac093d9389d58bcc6c537025675e9
MohammedJR <mohammed.j@hotelistan.com>
2023-02-15 15:32:37 +0300

    fix: click out side the modal colses it

--- src/components/CompanyRelatedInformation/CompanyRelatedInformation.tsx
+++ src/components/CompanyRelatedInformation/CompanyRelatedInformation.tsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from '../Modal/Modal';
 
 const CompanyRelatedInformation = () => {
+  const [ isChecked, setIsChecked ] = useState<boolean>(false)
+  const [ isModalOpen, setIsModalOpen ] = useState<boolean>(false)
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsChecked(e.target.checked)
+    setIsModalOpen(true)
+  }
+
+  const closeModal = () => {
+    if(isModalOpen){
+      setIsModalOpen(false)
+    }
+  }
+
   return (
     <div className='container font-poppins'>
       <p className='font-bold py-6'>English</p>
@@ -24,6 +38,13 @@ const CompanyRelatedInformation = () => {
       <p className='mb-4  text-sm'>  
         One word of caution: make sure your client knows that lorem ipsum is filler text. You don't want them wondering why you filled their website with a foreign language, and you certainly don't want anyone prematurely publishing it.
       </p>
+      <div className='w-full border border-orange-400'>
+          <label>
+            <input id="termsOfService" type='checkbox' className='mr-4 rounded-full' checked={isChecked} onChange={handleChange}/>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam corporis sint quia minus facilis itaque? Molestias placeat, eligendi consequuntur ut laborum voluptatum aliquam quam debitis. Eveniet nostrum fugit inventore nisi. 
+          </label>
+          <Modal show={isModalOpen} OnCloseModal={closeModal} children={<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo tempora molestiae vitae eaque voluptates aspernatur reprehenderit obcaecati doloremque? Doloremque, maxime.</p>} />
+      </div>
     </div>
   )
 }
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, useEffect, useRef } from 'react';
 
 interface ModalType {
     show: boolean,
@@ -7,18 +7,39 @@ interface ModalType {
 }
 
 const Modal: FC<ModalType> = ({ show, children, OnCloseModal }) => {
-  const showHideClassName = show ? "modal display-block" : "modal display-none";
+    const showHideClassName = show ? "modal display-block" : "modal display-none";
+    const overlayRef = useRef<HTMLDivElement>(null)
+    const modalPanelRef = useRef<HTMLDivElement>(null)
+    const closeModalOnClickOutside = (event: MouseEvent) => {
+        
+        console.log('handle click outside called')
+        if (!overlayRef.current || modalPanelRef?.current?.contains(event.target as Node) ) return;
+        if ( overlayRef.current.contains(event.target as Node) ) {
+            OnCloseModal()
+        }
+      };
+    
+      useEffect(() => {
+        if(!show) return
+        if (closeModalOnClickOutside) {
+          document.removeEventListener("click", closeModalOnClickOutside);
+        }
+         document.addEventListener("click", closeModalOnClickOutside);
+        return () => {
+          document.removeEventListener("click", closeModalOnClickOutside);
+        };
+      }, [show]);
 
-  return (
-    <div className={`fixed top-0 left-0 w-full h-screen bg-black bg-opacity-25 transition-all duration-500 ${show ? 'block opacity-100' : 'opacity-0 hidden'}`}>
-      <section className="fixed bg-white w-4/5 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-8 transition-all">
-        {children}
-        <button type="button" onClick={OnCloseModal}>
-          Close
-        </button>
-      </section>
-    </div>
-  );
+    return (
+        <div className={`fixed top-0 left-0 w-full h-screen bg-black bg-opacity-25 transition-all duration-500 ${show ? 'block opacity-100' : 'opacity-0 hidden'}`} ref={overlayRef}>
+        <section className="fixed bg-white w-4/5 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-8 transition-all" ref={modalPanelRef}>
+            {children}
+            <button type="button" onClick={OnCloseModal}>
+            Close
+            </button>
+        </section>
+        </div>
+    );
 };
 
 export default Modal
