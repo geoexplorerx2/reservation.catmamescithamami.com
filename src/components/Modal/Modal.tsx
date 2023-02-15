d0405447e34bf9a4bc989a3970cb93bc9a3a7e15
MohammedJR <mohammed.j@hotelistan.com>
2023-02-15 17:01:07 +0300

    CMH-27

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -3,10 +3,11 @@ import { FC, ReactNode, useEffect, useRef } from 'react';
 interface ModalType {
     show: boolean,
     children: ReactNode,
-    OnCloseModal: () => void
+    closeModal: () => void,
+    acceptAndclose?: () => void
 }
 
-const Modal: FC<ModalType> = ({ show, children, OnCloseModal }) => {
+const Modal: FC<ModalType> = ({ show, children, closeModal }) => {
     const showHideClassName = show ? "modal display-block" : "modal display-none";
     const overlayRef = useRef<HTMLDivElement>(null)
     const modalPanelRef = useRef<HTMLDivElement>(null)
@@ -15,7 +16,7 @@ const Modal: FC<ModalType> = ({ show, children, OnCloseModal }) => {
         console.log('handle click outside called')
         if (!overlayRef.current || modalPanelRef?.current?.contains(event.target as Node) ) return;
         if ( overlayRef.current.contains(event.target as Node) ) {
-            OnCloseModal()
+            closeModal()
         }
       };
     
@@ -34,9 +35,6 @@ const Modal: FC<ModalType> = ({ show, children, OnCloseModal }) => {
         <div className={`fixed top-0 left-0 w-full h-screen bg-black bg-opacity-25 transition-all duration-500 ${show ? 'block opacity-100' : 'opacity-0 hidden'}`} ref={overlayRef}>
         <section className="fixed bg-white w-4/5 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-8 transition-all" ref={modalPanelRef}>
             {children}
-            <button type="button" onClick={OnCloseModal}>
-            Close
-            </button>
         </section>
         </div>
     );
