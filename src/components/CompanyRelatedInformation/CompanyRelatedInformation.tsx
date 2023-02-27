3f7bc3f3a10e4625f71807cb9e666c2c2ac8277d
MohammedJR <mohammed.j@hotelistan.com>
2023-02-27 15:33:50 +0300

    feat: translation for terms of service

--- src/components/CompanyRelatedInformation/CompanyRelatedInformation.tsx
+++ src/components/CompanyRelatedInformation/CompanyRelatedInformation.tsx
@@ -40,7 +40,7 @@ const CompanyRelatedInformation: FC<CompanyRelatedInformationType> = (props) => 
 
     return (
       <div className='flex flex-col'>
-<p className='font-bold py-6'>Terms of Service</p>
+<p className='font-bold py-6'>{t("TERMS_OF_SERVICE")}</p>
       <p className='mb-4 text-sm'>
         So when is it okay to use lorem ipsum? First, lorem ipsum works well for staging. It's like the props in a furniture store—filler text makes it look like someone is home. The same Wordpress template might eventually be home to a fitness blog, a photography website, or the online journal of a cupcake fanatic. Lorem ipsum helps them imagine what the lived-in website might look like.
       </p>
